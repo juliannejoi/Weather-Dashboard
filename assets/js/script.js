809c128e99dadb0ec9862754ad22a8752d6b8150
unknown <joispersonalemail@gmail.com>
2023-01-21 15:06:02 -0800

    updated js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
+// GIVEN a weather dashboard with form inputs
 
+// WHEN user searches for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+var searchButton = document.querySelector('.searchButton');
+searchButton.addEventListener('click', getWeather);
+
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+function getWeather (event){
+    event.preventDefault();
+    var search-input
+}
+
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
